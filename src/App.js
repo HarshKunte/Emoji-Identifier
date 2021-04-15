928ebb7d3a7c05457e10305032d423e74deec2da
HarshKunte <harshkunte99@gmail.com>
2021-04-15 14:30:38 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import react , {useState} from 'react';
+import './App.css'
+
 
 function App() {
+  const emojiCollection = {
+    "😊": "Smiling Emoji",
+    "😂": "Laughining Emoji",
+    "😍": "Loving Emoji",
+    "😎": "Cool Emoji",
+    "😔": "Sad Emoji",
+    "😭": "Crying Emoji",
+    "😮": "Shocked Emoji",
+    "😉": "Winking Face",
+    "😚": "Kissing Face with Closed Eyes",
+"😙": "Kissing Face with Smiling Eyes",
+
+"😀": "Grinning Face",
+"😃": "Grinning Face with Big Eyes",
+"😄": "Grinning Face with Smiling Eyes",
+"😁": "Beaming Face with Smiling Eyes",
+"😆": "Grinning Squinting Face",
+"😅": "Grinning Face with Sweat",
+  }
+  const emojis = Object.keys(emojiCollection);
+
+   const [meaning , setMeaning] = useState("Result")
+
+  function emojiInputHandler(e) {
+      if(emojiCollection[e.target.value]===undefined)
+      setMeaning("Can't identify this emoji, use one of the emojis shown below")
+      else
+    setMeaning(emojiCollection[e.target.value]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <input type="text" onChange={emojiInputHandler} placeholder="Enter one of the emoji's shown below "/>
+       <h1>{meaning}</h1>
+       <div>
+       {emojis.map((e) => (
+        <span
+          key={e}
+          style={{ fontSize: "30px", margin: "5px", cursor: "pointer" }}
         >
-          Learn React
-        </a>
-      </header>
+          {e}
+        </span>
+          ))}
+          </div>
     </div>
   );
 }
